b1d85a0933e6a241051cf8d109bbe136db066ce9
Austeja <sketeryteausteja@gmail.com>
2024-03-15 17:42:04 +0100

    commit to changes

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+import { posts } from '/dataTransfer.js'
+
+const mainContainer = document.getElementById("main-container")
+
+function renderPosts(){
+    mainContainer.innerHTML = ``
+    for (let i = 0; i < posts.length; i++){
+        let postContent = `
+            <section class="container">
+                    
+            <div class="user-info">
+                    <img class="avatar" src=${posts[i].avatar}>
+                    <div>
+                        <h1 class="name">${posts[i].name}</h1>
+                        <h2 class="location">${posts[i].location}</h2>
+                    </div>
+            </div>
+            
+            <img class="container" id="post-img" src=${posts[i].post}>
+            
+            <div class="engagement-section">
+                <div class="icons">
+                    <img id="heart-icon" src="images/icon-heart.png">
+                    <img id="comment-icon" src="images/icon-comment.png">
+                    <img id="dm-icon" src="images/icon-dm.png">
+                </div>
+                <p id="like-count-el">${posts[i].likes} likes</p>
+                <p class="caption-section">
+                    <span class="username">${posts[i].username}</span>
+                    <span class="caption">${posts[i].comment}</span>
+                </p>
+            </div>`
+            if (i < posts.length - 1){
+                postContent += `<div class="grey-space"></div>`
+            }
+            postContent += `</section>`
+            mainContainer.innerHTML += postContent
     }
-]
+}
 
-const heartIcon = document.getElementById("heart-icon")
-const postImage = document.getElementById("post-img")
-let likeCountEl = document.getElementById("like-count-el")
-let likeCount = 21
-
-heartIcon.addEventListener("click", function(){
-    likeCount += 1
-    likeCountEl.textContent = `${likeCount} likes`
-})
-
-postImage.addEventListener("dblclick", function(){
-    likeCount += 1
-    likeCountEl.textContent = `${likeCount} likes`
-})
+renderPosts()
